911e1a711e9b8c3571bbd20a9125b6e75788b69b
Vincent Gim <vgim@sateva.com.au>
2017-07-03 22:42:11 +0800

    PRAMP: Session 2 attempt solution

---
+++ session-2-no-of-paths/NumberOfPaths/NumberOfPaths/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        numOfPathsToDest(n);
+    }
+
+    static void numOfPathsToDest(int n) // n == 5
+    {
+        var result = travel(0, 0, n);
+        Console.WriteLine(result);
+    }
+
+
+    static int travel(int i, int j, int n)
+    {
+        if (i == n - 1 && j == n - 1)
+        {
+            return 1;
+        }
+
+        var count = 0;
+        if (i >= j)
+        {
+            if (i < n - 1)
+            {
+                count += travel(i + 1, j, n);
+            }
+
+            if (j < n - 1)
+            {
+                count += travel(i, j + 1, n);
+            }
+        }
+        return count;
+    }
+}
