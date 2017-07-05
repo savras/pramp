ffff7b3389d83cc2ee455b497200bceb3d06fc3a
Vincent Gim <vgim@sateva.com.au>
2017-07-05 20:48:08 +0800

    PRAMP-2: Added dynamic programming solution

--- session-2-no-of-paths/NumberOfPaths/NumberOfPaths/Program.cs
+++ session-2-no-of-paths/NumberOfPaths/NumberOfPaths/Program.cs
@@ -5,10 +5,31 @@ class Solution
     static void Main(string[] args)
     {
         var n = Convert.ToInt32(Console.ReadLine());
-        numOfPathsToDest(n);
+        //numOfPathsToDest(n);
+        numOfPathsToDestDp(n);
     }
 
-    static void numOfPathsToDest(int n) // n == 5
+    static void numOfPathsToDestDp(int n)
+    {
+        var dpArr = new int[n, n];
+
+        for (var i = 0; i < n; i++)
+        {
+            dpArr[0, i] = 1;
+        }
+
+        for (var i = 1; i < n; i++)
+        {
+            for (var j = i; j < n; j++)
+            {
+                dpArr[i, j] = dpArr[i - 1, j] + dpArr[i, j - 1];
+            }
+        }
+
+        Console.WriteLine(dpArr[n - 1, n - 1]);
+    }
+
+    static void numOfPathsToDest(int n) // Test: n == 5
     {
         var result = travel(0, 0, n);
         Console.WriteLine(result);
